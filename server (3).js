6722c305d60474b92aed6cff98505070baec1989
harrisonsavage-eng <harrison.savage@education.nsw.gov.au>
2026-05-18 16:48:29 +1000

    Add files via upload

---
+++ server (3).js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(500); res.end('Error loading game'); return; }
+    res.writeHead(200, { 'Content-Type': 'text/html', 'Cache-Control': 'no-cache' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log('Panda Wars running on port ' + PORT);
+});
